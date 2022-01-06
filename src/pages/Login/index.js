c8bb18e6f665c45f4fec2b1d902bd2b1cde5d680
alexjje25 <83323289+alexjje25@users.noreply.github.com>
2022-01-06 11:34:10 -0300

    testes
    testes

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
+//import axios from '../../services/axios';
 
 export default function Login() {
+    function handleClick(e){
+        e.preventDefault();
+        console.log('Botao clicado');
+    }
     return (
-        
         <Container>
         <Title>
             TELA DE LOGIN
         </Title>
         <Paragrafo>Lorem BE ol grto hjua jhhusa </Paragrafo>
-        <button type='button'>Cadastrar</button>
+        <button type='button' onClick={handleClick}>Cadastrar</button>
         </Container>
     );
 }
