24b9ed595bc283d8dbd6034774a03a601f0dabc9
alexjje25 <83323289+alexjje25@users.noreply.github.com>
2022-01-06 10:21:10 -0300

    Inserindo redux
    Inserindo redux

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import React from 'react';
 import { Router } from 'react-router-dom'
 import Routes from './routes'
 import history from './services/history';
+//redux
+import { Provider } from 'react-redux'
+import store from './store'
 
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
 
 function App() {
   return ( 
+    <Provider store={store}>
     <Router history={history}>
     <Header />
     <Routes />
     <GlobalStyles/>
     </Router>
+    </Provider>
     
   );
 }
